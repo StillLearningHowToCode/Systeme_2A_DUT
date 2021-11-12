c09ffcaa2cbc8fec9414621fcad6c40d1288d22c
Léane MULLER <42692272+AyatoKirishima@users.noreply.github.com>
2021-11-12 15:00:10 +0100

    Add files via upload

---
+++ 4_Tableau_dynamique.c
@@ -0,0 +1,31 @@
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <string.h>  
+#include <fcntl.h>  
+#include <time.h>
+
+
+// Main
+int main(int argc, char *argv[] ) { 
+    int taille = 0;
+
+    scanf ("%d", &taille);
+    printf ("%d\n", taille);
+
+    /*float* p;
+    p=( float*) malloc(n*sizeof(float ));
+    if(p!=NULL)
+    {
+    //tout s’est bien passe: p pointe le debut du tableau alloué
+    }
+    else
+    {
+    }*/
+    /* Initialise le générateur aléatoires
+    srand (time(NULL));
+    printf ("First number: %d\n", rand()%100);
+    printf ("First number: %d\n", rand()%100);
+    printf ("First number: %d\n", rand()%100);*/
+
+    //return 0;
+}
