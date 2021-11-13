8064d28e7ab5e015544edfcfd24a00f1986197c1
Blue <leanefelis@gmail.com>
2021-11-13 13:28:25 +0100

    redimmensionenr tableau ok

--- 5_Tableau_changement_taille.c
+++ 5_Tableau_changement_taille.c
@@ -5,37 +5,20 @@
 #include <time.h>
 
 // Entêtes
-void demandeSaisie(taille);
-void afficher(taille);
+// int demandeSaisie();
+// int afficher(int taille);
 
 // Main
 int main(int argc, char *argv[])
 {
     // Variables
-    int taille = 0;
+    int taille = 20;
     char rep;
 
-    demandeSaisie(taille);
-    afficher(taille);
-    printf("Voulez vous redimmensionner le tableau ? o/n");
-    scanf("%s", &rep);
-    if (rep == ('o' || 'O'))
-    {
-        printf("Let's gooooo \n");
-        demandeSaisie(taille);
-        afficher(taille);
-    }
-    else
-    {
-        printf("See you later then!");
-    }
-    demandeSaisie(taille);
-    afficher(taille);
-    return 0;
-}
-
-void afficher(taille)
-{
+    printf("Saisir la taille du tableau :");
+    scanf("%d", &taille); //taille est définie ici, pas besoin de la réattribuer
+    printf("Taille du tableau : %d\n", taille);
+    // Affichage
     printf("Affichage du tableau :\n");
     int tab[taille]; // tableau d'entier car int
     for (int i = 0; i < taille; i++)
@@ -43,11 +26,41 @@ void afficher(taille)
         printf("%d", tab[i]);
         printf("\n");
     }
+    printf("Voulez vous redimmensionner le tableau ? o/n\n");
+    scanf("%s", &rep);
+    getchar();
+    if ((strcmp(&rep, "o") ==0))
+    //(strcmp(rep, ('o' || 'O'))==0)
+    {
+        printf("Let's gooooo !\n");
+        printf("Saisir la taille du tableau :");
+        scanf("%d", &taille); //taille est définie ici, pas besoin de la réattribuer
+        printf("Taille du tableau : %d\n", taille);
+    }
+    else
+    {
+        printf("Fin du programme");
+    }
+    return 0;
 }
 
-void demandeSaisie(taille)
-{
-    printf("Saisir la taille du tableau :");
-    scanf("%d", &taille); //taille est définie ici, pas besoin de la réattribuer
-    printf("Taille du tableau : %d\n", taille);
-}
+// int afficher(int taille)
+// {
+//     //int taille = 0;
+//     printf("Affichage du tableau :\n");
+//     int tab[taille]; // tableau d'entier car int
+//     for (int i = 0; i < taille; i++)
+//     {
+//         printf("%d", tab[i]);
+//         printf("\n");
+//     }
+//     return taille;
+// }
+
+// int demandeSaisie(int taille)
+// {
+//     printf("Saisir la taille du tableau :");
+//     scanf("%d", &taille); //taille est définie ici, pas besoin de la réattribuer
+//     printf("Taille du tableau : %d\n", taille);
+//     return taille;
+// }
